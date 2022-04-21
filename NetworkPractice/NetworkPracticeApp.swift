c37d183e5c43a35a8d9a57f3e0d74dc8d12944a2
MickhailP <mich_p@inbox.ru>
2022-04-21 18:49:29 +0300

    Initial Commit

---
+++ NetworkPractice/NetworkPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkPracticeApp.swift
+//  NetworkPractice
+//
+//  Created by Миша Перевозчиков on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
